71a9f174a1ef4803f3d05fb013167f88dce95446
markflerko <markflerko@gmail.com>
2022-06-17 19:39:46 +0400

    refactor: remove excessive mw

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,24 @@
+import http from 'http';
 import dotenv from 'dotenv';
-import app from './app';
+import requestListener from './requestListener';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`));
+const app = http.createServer(requestListener);
+
+app.listen(PORT, () =>
+  console.log(`App is running on http://localhost:${PORT}`)
+);
+
+process.on('uncaughtException', (error) => {
+  console.log(`captured error: ${error.message}`);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason: { message: string }) => {
+  console.log(`Unhandled rejection detected: ${reason.message}`);
+});
+
+export default app;
